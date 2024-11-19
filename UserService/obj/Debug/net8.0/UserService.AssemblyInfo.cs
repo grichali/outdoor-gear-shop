72ddeb7eb99e0304f576c2993dd73b0ed6cff47f
youssef belkhiri <belkhiriyoussef33@gmail.com>
2024-11-19 16:18:44 +0100

    dbContext

--- UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797e0ead26b71f0f43b264553c1b921607ea4810")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048f07eb4d1a6c429242e08062fbd447bdd912bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
