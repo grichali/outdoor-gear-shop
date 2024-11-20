abb5fbfd6afcf2a7a0af94cd80fe972f1f66625a
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-11-20 02:11:55 +0100

    delete filextension

--- UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2259d2113d141f56154c1982d9bee9b70915c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb756a9a3188234ff3b7ae1cc3936e833f578a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
