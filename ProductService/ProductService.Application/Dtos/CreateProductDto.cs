f8b4f9485a3c06e1545a5d976d0f76d267fb5809
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-11-22 01:06:38 +0100

    start implementation of productservice services

--- ProductService/ProductService.Application/Dtos/CreateProductDto.cs
+++ ProductService/ProductService.Application/Dtos/CreateProductDto.cs
@@ -10,7 +10,6 @@ namespace ProductService.Application.Dtos
 {
     public class CreateProductDto
     {
-        public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
