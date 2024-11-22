e8e64b97af3858b273ae04f556913acc49df7cfa
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-11-22 18:00:49 +0100

    update dependencies

--- ProductService/ProductService.Application/obj/Debug/net8.0/ProductService.Application.AssemblyInfo.cs
+++ ProductService/ProductService.Application/obj/Debug/net8.0/ProductService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b4f9485a3c06e1545a5d976d0f76d267fb5809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66611f8afa2d57267abd2f60055e13c6819bc964")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
